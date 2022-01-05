3d8bff86d7da6050c52bf034aa82f651c4abd773
Márton Bán <30813844+martonban@users.noreply.github.com>
2022-01-05 20:24:48 +0100

    Section 1

---
+++ Section 1 Intro - First Concepts/main.cpp
@@ -0,0 +1,7 @@
+#include<cstdio>
+
+int main(){
+    printf("Hello C++ \n");
+    printf("Remelem sikerul megtanulnom a szamitogepes grafikat \n");
+    printf("Hat ez a kurzus elegge laza eddig, csodat kell csinalni, hogy legyen is ertelme... \n");
+}
