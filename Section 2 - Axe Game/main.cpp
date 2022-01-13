26526d0b94e4e5b30322d175bb2ff71168719443
Márton Bán <30813844+martonban@users.noreply.github.com>
2022-01-14 00:21:32 +0100

    init

---
+++ Section 2 - Axe Game/main.cpp
@@ -0,0 +1,13 @@
+#include "raylib.h"
+
+int main(){
+    int width;
+    float root_beer;
+    root_beer = 1.99;
+    double cheese_burger;
+    cheese_burger = 5.99;
+    bool shouldHaveLunch;
+    shouldHaveLunch = true;
+    width = 350;
+    InitWindow(width, 200, "MyFirstC++Game");
+}
